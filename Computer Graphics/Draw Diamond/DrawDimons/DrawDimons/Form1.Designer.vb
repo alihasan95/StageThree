200c58c6973225f765144533f4d5b565ab0d6761
Mahmood Ali <Mahmood Ali>
2016-12-01 23:39:23 +0300

    Draw Diamond

---
+++ Computer Graphics/Draw Diamond/DrawDimons/DrawDimons/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Pc_DrawnArea = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.Pc_DrawnArea, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Pc_DrawnArea
+        '
+        Me.Pc_DrawnArea.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Pc_DrawnArea.InitialImage = Global.DrawDimons.My.Resources.Resources.Zipline_48px
+        Me.Pc_DrawnArea.Location = New System.Drawing.Point(0, 63)
+        Me.Pc_DrawnArea.Name = "Pc_DrawnArea"
+        Me.Pc_DrawnArea.Size = New System.Drawing.Size(658, 285)
+        Me.Pc_DrawnArea.TabIndex = 0
+        Me.Pc_DrawnArea.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(99, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(545, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(239, 373)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(152, 35)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(653, 412)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Pc_DrawnArea)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Pc_DrawnArea, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Pc_DrawnArea As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Computer Graphics/Draw Diamond/DrawDimons/DrawDimons/Form1.vb
@@ -0,0 +1,265 @@
+﻿Public Class Form1
+
+    Private Point1, Point2 As Point
+
+
+
+    Private Sub Pc_DrawnArea_MouseDown(sender As Object, e As MouseEventArgs) Handles Pc_DrawnArea.MouseDown
+        Point1 = e.Location
+
+        Label1.Text = Point1.ToString()
+    End Sub
+
+    Private Sub Pc_DrawnArea_MouseUp(sender As Object, e As MouseEventArgs) Handles Pc_DrawnArea.MouseUp
+        Point2 = e.Location
+
+        Label2.Text = Point2.ToString()
+
+        DrawLine(Point1, Point2)
+
+    End Sub
+
+
+    Private Sub DrawnLineDirect(p1 As Point, p2 As Point)
+
+        Dim graph As Graphics = Pc_DrawnArea.CreateGraphics()
+
+
+
+        Dim length, Dy, Dx, Xinc, Yinc As Integer
+
+        Dy = p2.Y - p1.Y
+        Dx = p2.X - p1.X
+
+        length = Math.Max(Math.Abs(Dy), Math.Abs(Dx))
+
+        Xinc = Dx / length
+        Yinc = Dy / length
+
+
+        Dim Img As New Bitmap(1, 1)
+        Img.SetPixel(0, 0, Color.Green)
+
+
+        Dim x, y As Integer
+        x = p1.X
+        y = p1.Y
+
+        For i = 1 To length
+
+            graph.DrawImage(Img, x, y)
+            x += Xinc
+            y += Yinc
+
+        Next
+
+
+
+    End Sub
+
+
+    Private Sub BresenhumLine(p1 As Point, p2 As Point)
+
+        Dim graph As Graphics = Pc_DrawnArea.CreateGraphics()
+
+
+
+        Dim Dy, Dx, P, s1, s2, temp, change
+
+        Dy = Math.Abs(p2.Y - p1.Y)
+        Dx = Math.Abs(p2.X - p1.X)
+
+        P = 2 * (Dy - Dx)
+
+        s1 = Math.Sign(p2.X - p1.X)
+        s2 = Math.Sign(p2.Y - p2.Y)
+
+
+        Dim Img As New Bitmap(1, 1)
+        Img.SetPixel(0, 0, Color.Green)
+
+
+
+        If (Dy > Dx) Then
+            temp = Dx
+            Dx = Dy
+            Dy = Dx
+            change = 1
+
+        Else
+            change = 0
+        End If
+
+
+        Dim x, y As Integer
+
+        For i = 1 To Dx
+            If (P > 0) Then
+                If (change = 1) Then
+                    x = x + s1
+                Else
+                    y = y + s2
+                End If
+            Else
+                P = P + 2 * Dy
+            End If
+            If (change = 1) Then
+                y = y + s2
+            Else
+                x = x + s1
+            End If
+
+            graph.DrawImage(Img, x, y)
+        Next
+
+
+
+
+    End Sub
+
+
+
+    Private Sub DrawLineDirect(Point1 As Point, Point2 As Point)
+
+        Dim i, Dx, Dy, B, m, New_Y, New_X As Integer
+
+
+        Dx = Point2.X - Point1.X
+        Dy = Point2.Y - Point1.Y
+
+        If (Dx = 0) Then
+            m = 0
+        Else
+            m = Dy / Dx
+        End If
+
+
+
+        Dim g As Graphics = Pc_DrawnArea.CreateGraphics()
+        Dim Img As New Bitmap(1, 1)
+        Img.SetPixel(0, 0, Color.White)
+
+
+        B = Point1.Y - (m * Point1.X)
+
+        If (Dy > Dx) Then
+
+
+
+            For i = Point1.X + 1 To Point2.X
+
+                New_Y = B + m * i
+                g.DrawImage(Img, i, Math.Round(New_Y))
+
+            Next
+
+            Console.WriteLine("First Dy>DX")
+        Else
+
+            If (m = 0) Then
+                m = 1
+            End If
+            For i = Point1.Y + 1 To Point2.Y
+
+                New_X = (i - B) / m
+
+                g.DrawImage(Img, Math.Round(New_X), i)
+
+            Next
+
+
+            Console.WriteLine("Second  DX>DY")
+        End If
+
+
+
+
+
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Point1 = New Point(0, 0)
+        Point2 = New Point(0, 0)
+
+    End Sub
+
+
+    Private Sub DrawLine(p1 As Point, p2 As Point)
+
+        Dim graph As Graphics = Pc_DrawnArea.CreateGraphics()
+
+        graph.DrawLine(Pens.Green, p1, p2)
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim graph As Graphics = Pc_DrawnArea.CreateGraphics()
+
+        Dim mHeight = Pc_DrawnArea.Height / 2
+
+
+        Dim startPoint As Point = New Point(300, Pc_DrawnArea.Height - 10)
+        Dim p2 As Point = New Point(400, mHeight)
+
+        DrawLine(startPoint, p2)
+
+
+        p2 = New Point(370, mHeight + 10)
+        DrawLine(startPoint, p2)
+
+
+        p2 = New Point(230, mHeight + 10)
+        DrawLine(startPoint, p2)
+
+
+        p2 = New Point(200, mHeight)
+        DrawLine(startPoint, p2)
+
+        'Long line 
+        p2 = New Point(350, mHeight - 20)
+        DrawLine(startPoint, p2)
+
+        p2 = New Point(250, mHeight - 20)
+        DrawLine(startPoint, p2)
+
+
+        ' level two 
+        'line above base line 
+
+        Dim p1 As Point = New Point(200, mHeight)
+        p2 = New Point(230, mHeight + 10)
+        DrawLine(p1, p2)
+
+        p1 = New Point(230, mHeight + 10)
+        p2 = New Point(370, mHeight + 10)
+        DrawLine(p1, p2)
+
+        p1 = New Point(370, mHeight + 10)
+        p2 = New Point(400, mHeight)
+        DrawLine(p1, p2)
+
+
+        'level there
+
+        p1 = New Point(400, mHeight)
+        p2 = New Point(350, mHeight - 20)
+        DrawLine(p1, p2)
+
+        p1 = New Point(200, mHeight)
+        p2 = New Point(250, mHeight - 20)
+        DrawLine(p1, p2)
+
+        p1 = New Point(250, mHeight - 20)
+        p2 = New Point(350, mHeight - 20)
+        DrawLine(p1, p2)
+
+
+
+    End Sub
+
+    Private Sub Pc_DrawnArea_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Pc_DrawnArea.MouseDoubleClick
+        Pc_DrawnArea.Image = Nothing
+    End Sub
+End Class
